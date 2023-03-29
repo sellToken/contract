a81de10b0b87fc3e009123d887935e84917ee4bf
sellToken <128102424+sellToken@users.noreply.github.com>
2023-03-29 23:16:21 +0800

    Update sellTokenNew8.18-1.sol

--- sellTokenNew8.18-1.sol
+++ sellTokenNew8.18-1.sol
@@ -357,8 +357,8 @@ contract Minerals is Ownable{
     function setPool(address token,address token1,uint coin,uint _day,uint isPool)payable public{
         require(token1 == _WBNB || token1==_USDT);
         if(isPool==0){
-          require(balanceOfLook[token] > 0 || isPool >0);
-          require(Users[_msgSender()][token].value == 0 || isPool >0);
+          require(balanceOfLook[token] > 0);
+          require(Users[_msgSender()][token].value == 0);
           bool isok=IERC20(token).transferFrom(_msgSender(),address(this),coin);
           require(isok);
           balanceOf[token]+=coin;
